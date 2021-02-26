d7d15522f2c3c2c7a376afbaee97a664c854e0fe
Matt712 <47025888+matt712@users.noreply.github.com>
2021-02-26 15:10:31 +0000

    dependecy update

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,11 +6,11 @@ import sbt._
 object AppDependencies {
 
   val compile = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-backend-play-27"  % "3.2.0"
+    "uk.gov.hmrc"             %% "bootstrap-backend-play-27"  % "3.4.0"
   )
 
   val test = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-test-play-27"   % "3.2.0" % Test,
+    "uk.gov.hmrc"             %% "bootstrap-test-play-27"   % "3.4.0" % Test,
     "org.scalatest"           %% "scalatest"                % "3.2.3"  % Test,
     "com.typesafe.play"       %% "play-test"                % current  % Test,
     "com.vladsch.flexmark"    %  "flexmark-all"             % "0.36.8" % "test, it",
