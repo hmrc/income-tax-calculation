09b9fe28efbc476c80362fb662124ecef15538c0
dnyuar <60218795+dnyuar@users.noreply.github.com>
2021-03-04 13:36:42 +0000

    [SASS-410] Changed debug logs to info logs

--- test/controllers/predicates/AuthorisedActionSpec.scala
+++ test/controllers/predicates/AuthorisedActionSpec.scala
@@ -108,7 +108,6 @@ class AuthorisedActionSpec extends TestSuite {
     ".agentAuthenticated" should {
 
       val block: User[AnyContent] => Future[Result] = user => Future.successful(Ok(s"${user.mtditid} ${user.arn.get}"))
-      val arn = "0987654321"
 
       "perform the block action" when {
 
