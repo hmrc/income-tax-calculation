f570ac93f683c16183b24bbaaa933880f96998a4
Matt712 <47025888+matt712@users.noreply.github.com>
2021-03-08 16:03:02 +0000

    Added return type to method

--- test/connectors/DesConnectorSpec.scala
+++ test/connectors/DesConnectorSpec.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package connectors
 
 import config.AppConfig
@@ -8,7 +24,7 @@ import uk.gov.hmrc.http.logging.Authorization
 class DesConnectorSpec extends TestSuite{
 
   class FakeConnector(override val appConfig: AppConfig) extends DesConnector {
-    def headerCarrierTest(hc: HeaderCarrier) = desHeaderCarrier(hc)
+    def headerCarrierTest(hc: HeaderCarrier): HeaderCarrier = desHeaderCarrier(hc)
   }
   val connector = new FakeConnector(appConfig = mockAppConfig)
 
