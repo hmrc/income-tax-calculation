0fd874d68d963fccf2f51d0cfbcaf3e79cddc115
Joey Moore <22170468+joeyymoore@users.noreply.github.com>
2021-03-16 10:28:08 +0000

    updated logMessage to only check for correlationId

--- app/connectors/httpParsers/LiabilityCalculationHttpParser.scala
+++ app/connectors/httpParsers/LiabilityCalculationHttpParser.scala
@@ -52,14 +52,11 @@ object LiabilityCalculationHttpParser {
   }
 
   private def logMessage(response:HttpResponse): Option[String] = {
-
-    val addedId = (response.header("CorrelationId"), response.header("sessionId")) match {
-      case (Some(id), _) => s" CorrelationId: $id"
-      case (_, Some(id)) => s" SessionId: $id"
+    val correlationId = response.header("CorrelationId") match {
+      case Some(id) => s" CorrelationId: $id"
       case _ => ""
     }
-
-    Some(s"[LiabilityCalculationHttpParser][read] Received ${response.status} from DES. Body:${response.body}." + addedId)
+    Some(s"[LiabilityCalculationHttpParser][read] Received ${response.status} from DES. Body:${response.body}." + correlationId)
   }
 
   private def handleDESError(response: HttpResponse, statusOverride: Option[Int] = None): LiabilityCalculationResponse = {
