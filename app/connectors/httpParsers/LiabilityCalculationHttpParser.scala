a4a39060f3f6181862090dc278b5c27b586106d1
juamalBlackman <63247344+juamalBlackman@users.noreply.github.com>
2021-06-18 13:24:14 +0100

    [SASS-900] added connector 422/UNPROCESSSABLE ENTITY (#22)
    * SASS-900 added connector 422/UNPROCESSSABLE ENTITY

--- app/connectors/httpParsers/LiabilityCalculationHttpParser.scala
+++ app/connectors/httpParsers/LiabilityCalculationHttpParser.scala
@@ -40,7 +40,7 @@ object LiabilityCalculationHttpParser extends DESParser {
         case SERVICE_UNAVAILABLE =>
           pagerDutyLog(SERVICE_UNAVAILABLE_FROM_DES, logMessage(response))
           handleDESError(response)
-        case BAD_REQUEST | NOT_FOUND | CONFLICT =>
+        case BAD_REQUEST | NOT_FOUND | CONFLICT | UNPROCESSABLE_ENTITY =>
           pagerDutyLog(FOURXX_RESPONSE_FROM_DES, logMessage(response))
           handleDESError(response)
         case _ =>
