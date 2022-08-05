260321ea12419ebe93a5bb9847c3cb46166ceeac
John Kelly <60345872+jhkldev@users.noreply.github.com>
2022-08-05 10:42:03 +0100

    SASS-3230: Update dependencies in income-tax-calculation

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -19,16 +19,16 @@ import sbt._
 
 object AppDependencies {
 
-  private val bootstrapPlay28Version = "6.3.0"
+  private val bootstrapPlay28Version = "6.4.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"             %% "bootstrap-backend-play-28"  % bootstrapPlay28Version,
-    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-28"         % "0.63.0",
+    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-28"         % "0.68.0",
     "com.fasterxml.jackson.module" %% "jackson-module-scala"  % "2.12.2"
   )
   val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"             %% "bootstrap-test-play-28"   % bootstrapPlay28Version % "test, it",
-    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-28"  % "0.63.0"               % "test, it",
+    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-28"  % "0.68.0"               % "test, it",
     "org.scalatest"           %% "scalatest"                % "3.2.9"                % "test, it",
     "com.typesafe.play"       %% "play-test"                % current                % "test, it",
     "org.pegdown"             %  "pegdown"                  % "1.6.0"                % "test, it",
