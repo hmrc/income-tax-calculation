5c451b1194d6aa3d1692dd7a6f46c60cf764a978
Andrii Kondiuk <94916773+andriikondiuk@users.noreply.github.com>
2022-10-07 16:44:01 +0100

    MISUV-4157 Create a new connector (#54)
    * MISUV-4157 Initial commint. Rename existing connector to legacy one
    
    * MISUV-4157 Add CalculationDetailsConnector for 1885 API
    
    * MISUV-4157 Addressing warnings and unused imports

--- it/connectors/CalculationDetailsConnectorLegacyISpec.scala
+++ it/connectors/CalculationDetailsConnectorLegacyISpec.scala
@@ -16,7 +16,6 @@
 
 package connectors
 
-import assets.GetCalculationDetailsConstants.{successCalcDetailsExpectedJsonFull, successModelFull}
 import com.github.tomakehurst.wiremock.http.HttpHeader
 import config.BackendAppConfig
 import helpers.WiremockSpec
@@ -25,12 +24,13 @@ import org.scalatest.matchers.must.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 import play.api.Configuration
 import play.api.http.Status._
+import assets.GetCalculationDetailsConstants.{successCalcDetailsExpectedJsonFull, successModelFull}
 import uk.gov.hmrc.http.{HeaderCarrier, HeaderNames, HttpClient, SessionId}
 import uk.gov.hmrc.play.bootstrap.config.ServicesConfig
 
-class CalculationDetailsConnectorISpec extends AnyWordSpec with WiremockSpec with Matchers {
+class CalculationDetailsConnectorLegacyISpec extends AnyWordSpec with WiremockSpec with Matchers {
 
-  lazy val connector: CalculationDetailsConnector = app.injector.instanceOf[CalculationDetailsConnector]
+  lazy val connector: CalculationDetailsConnectorLegacy = app.injector.instanceOf[CalculationDetailsConnectorLegacy]
 
   lazy val httpClient: HttpClient = app.injector.instanceOf[HttpClient]
 
@@ -38,16 +38,14 @@ class CalculationDetailsConnectorISpec extends AnyWordSpec with WiremockSpec wit
     override val ifBaseUrl: String = s"http://$ifHost:$wireMockPort"
   }
 
-  val taxYear: String = "23-24"
-
-  "CalculationDetailsConnector" should {
+  "CalculationDetailsConnectorLegacy" should {
 
     val appConfigWithInternalHost = appConfig("localhost")
-    val connector = new CalculationDetailsConnector(httpClient, appConfigWithInternalHost)
+    val connector = new CalculationDetailsConnectorLegacy(httpClient, appConfigWithInternalHost)
 
     val nino = "taxable_entity_id"
     val calculationId = "041f7e4d-87b9-4d4a-a296-3cfbdf92f7e2"
-    val url = s"/income-tax/view/calculations/liability/$taxYear/$nino/$calculationId"
+    val url = s"/income-tax/view/calculations/liability/$nino/$calculationId"
 
     "include internal headers" when {
       val headersSentToBenefits = Seq(
@@ -55,11 +53,11 @@ class CalculationDetailsConnectorISpec extends AnyWordSpec with WiremockSpec wit
       )
 
       "the host for DES is 'internal'" in {
-        implicit val hc: HeaderCarrier = HeaderCarrier(sessionId = Some(SessionId("sessionIdValue")))
+        implicit val headerCarrier: HeaderCarrier = HeaderCarrier(sessionId = Some(SessionId("sessionIdValue")))
 
         stubGetWithResponseBody(url, OK, successCalcDetailsExpectedJsonFull, headersSentToBenefits)
 
-        val result = await(connector.getCalculationDetails(taxYear, nino, calculationId)(hc))
+        val result = await(connector.getCalculationDetails(nino, calculationId)(headerCarrier))
 
         result mustBe Right(successModelFull)
       }
@@ -68,28 +66,29 @@ class CalculationDetailsConnectorISpec extends AnyWordSpec with WiremockSpec wit
     "handle errors" when {
       val desErrorBodyModel = DesErrorBodyModel("DES_CODE", "DES_REASON")
 
-      Seq(BAD_REQUEST, NOT_FOUND, CONFLICT, UNPROCESSABLE_ENTITY, INTERNAL_SERVER_ERROR, SERVICE_UNAVAILABLE).foreach { status =>
+      Seq(BAD_REQUEST, NOT_FOUND, CONFLICT, INTERNAL_SERVER_ERROR, SERVICE_UNAVAILABLE).foreach { status =>
         s"DES returns $status" in {
           val desError = DesErrorModel(status, desErrorBodyModel)
           implicit val hc: HeaderCarrier = HeaderCarrier()
 
           stubGetWithResponseBody(url, status, desError.toJson.toString)
 
-          val result = await(connector.getCalculationDetails(taxYear, nino, calculationId)(hc))
+          val result = await(connector.getCalculationDetails(nino, calculationId)(hc))
 
           result mustBe Left(desError)
         }
       }
-        "DES returns an unexpected error - 502 BadGateway" in {
-          val desError = DesErrorModel(BAD_GATEWAY, desErrorBodyModel)
-          implicit val hc: HeaderCarrier = HeaderCarrier()
 
-          stubGetWithResponseBody(url, BAD_GATEWAY, desError.toJson.toString())
+      "DES returns an unexpected error - 502 BadGateway" in {
+        val desError = DesErrorModel(BAD_GATEWAY, desErrorBodyModel)
+        implicit val hc: HeaderCarrier = HeaderCarrier()
 
-          val result = await(connector.getCalculationDetails(taxYear, nino, calculationId)(hc))
+        stubGetWithResponseBody(url, BAD_GATEWAY, desError.toJson.toString())
 
-          result mustBe Left(desError)
-        }
+        val result = await(connector.getCalculationDetails(nino, calculationId)(hc))
+
+        result mustBe Left(desError)
+      }
     }
   }
 }
